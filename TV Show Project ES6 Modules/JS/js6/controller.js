7ab471b15b214f5bcd08562a0a9d62afc0e8f9fe
BIT Student <classroom@bgit.rs>
2019-06-07 14:06:01 +0200

    dodata pretraga kroz input

--- TV Show Project ES6 Modules/JS/js6/controller.js
+++ TV Show Project ES6 Modules/JS/js6/controller.js
@@ -2,32 +2,39 @@ import * as ui from "./ui.js";
 import * as data from "./data.js";
 
 function init() {
-  data.getPopShows().then(function(shows) {
-    //ovde ide ono sto poslednji then iz date-e vraca kao promise
-    ui.renderShows(shows);
-    setupListenersOnShowClick();
-  });
-}
+  data.getPopShows()
+    .then(ui.renderShows)
+    .then(setupListenersOnShowClick)
+
+  //ovde ide ono sto poslednji then iz date-e vraca kao promise
+
+};
+
 
 function setupListenersOnShowClick() {
-  $(".tv-show").on("click", function() {
+  $(".tv-show").on("click", function () {
     const showId = $(this).attr("data-show-id");
 
-    data.getSingleShow(showId).then(function(showData) {
+    data.getSingleShow(showId).then(function (showData) {
       ui.renderSingleShow(showData);
     });
   });
 }
 
 function setupListenersOnKeybord() {
-  $(".input").on("keydown", function() {
-    const inputValue = $(this).val();
-    data.getDropDownList(inputValue).then(function(arr) {
-      console.log(arr);
-      });
-    });
+  $(".input").on("keypress", function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const inputValue = $(this).val();
+      data.getDropDownList(inputValue)
+        .then((inputValue) => {
+          ui.renderList(inputValue);
+          setupListenersOnShowClick();
+        })
+    };
   });
 }
+
 setupListenersOnKeybord();
 
 export { init, setupListenersOnShowClick, setupListenersOnKeybord };
--- TV Show Project ES6 Modules/JS/js6/data.js
+++ TV Show Project ES6 Modules/JS/js6/data.js
@@ -1,28 +1,27 @@
-// import { log } from "winjs";
 
 function getPopShows() {
   return fetch("http://api.tvmaze.com/shows")
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(shows) {
+    .then(function (shows) {
       shows.slice(0, 50);
       return shows;
     });
 }
 
 function getSingleShow(showId) {
-  return fetch(`http://api.tvmaze.com/shows/${showId}?embed[]=seasons&embed[]=cast`).then(function(response) {
+  return fetch(`http://api.tvmaze.com/shows/${showId}?embed[]=seasons&embed[]=cast`).then(function (response) {
     return response.json();
   });
 }
 
 function getDropDownList(inputValue) {
   return fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(arr) {
+    .then(function (arr) {
       arr.slice(0, 10);
       return arr;
     });
