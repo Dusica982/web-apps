ccf600c5189571f046d57dcdc173fcf5e1bc5e67
BIT Student <classroom@bgit.rs>
2019-06-14 09:37:34 +0200

    feature 2 finished

--- Week9/BitReactUser/src/shared/enitities/User.js
+++ Week9/BitReactUser/src/shared/enitities/User.js
@@ -1,22 +1,19 @@
 class User {
-  constructor(name, last, email, img, date) {
-    this.name = name;
-    this.last = last;
-    this.email = email;
-    this.img = img;
-    this.date = date;
-  }
-  hideEmail() {
-    return this.email.slice(0, 3);
-  }
-  setFullName() {
-    return this.name + " " + this.last;
-  }
-  setDate() {
-    return (
-      new Date(this.date).getMonth() + 1 + "-" + new Date(this.date).getDate() + "-" + new Date(this.date).getFullYear()
-    );
-  }
+    constructor(name, last, email, img, date) {
+        this.name = name;
+        this.last = last;
+        this.email = email;
+        this.img = img;
+        this.date = date;
+    }
+    hideEmail() {
+        return this.email.slice(0, 3)
+    }
+    setFullName() {
+        return this.name + " " + this.last
+    }
+    setDate() {
+        return this.date.slice(0, 10)
+    }
 }
-
-export { User };
+export { User }
--- Week9/BitReactUser/src/view/UserLists.js
+++ Week9/BitReactUser/src/view/UserLists.js
@@ -1,30 +1,28 @@
-import React from "react";
-import { User } from "../shared/enitities/User";
+import React from 'react';
+import { User } from '../shared/enitities/User';
 
-const UserLists = props => {
-  console.log(props.users);
 
-  const arrOfUsers = props.users.map((oneUser, i) => {
-    return (
-      <div className="user" key={i}>
-        <img src={oneUser.img} />
-        <div>
-          <p>{oneUser.setFullName()}</p>
-          <i className="fa fa-envelope" aria-hidden="true">
-            email:{oneUser.hideEmail()}
-          </i>
-          <p>
-            <i className="fa fa-birthday-cake" aria-hidden="true">
-              {oneUser.setDate()}
-            </i>
-          </p>
-        </div>
-      </div>
-    );
-  });
-  console.log(arrOfUsers);
+const UserLists = (props) => {
+    console.log(props.users)
 
-  return arrOfUsers;
-};
+    const arrOfUsers = props.users.map((oneUser, i) => {
+        return (
+
+            <div className="user" key={i}>
+                <img src={oneUser.img} />
+                <div>
+                    <p>{oneUser.setFullName()}</p>
+                    <i className="fa fa-envelope" aria-hidden="true">Email {oneUser.hideEmail()}</i>
+                    <p>
+                        <i className="fa fa-birthday-cake" aria-hidden="true">  {oneUser.setDate()}</i>
+                    </p>
+                </div>
+            </div>
+        )
+    })
+    console.log(arrOfUsers);
+
+    return arrOfUsers;
+}
 
 export default UserLists;
