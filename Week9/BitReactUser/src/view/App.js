1bb5b67794506e3da171ac4e9c5a61bdce46a1d3
dusica982 <dusica.antic@gmail.com>
2019-06-15 14:02:46 +0200

    reload on browser reload button

--- Week9/BitReactUser/src/view/App.js
+++ Week9/BitReactUser/src/view/App.js
@@ -1,53 +1,103 @@
-import React from 'react';
-import Header from './Header'
-import UserLists from './UserLists'
-import Footer from './Footer'
-import fetchUsers from '../shared/services/userService';
-import GridStructure from './GridStructure';
+import React from "react";
+import Header from "./Header";
+import UserLists from "./UserLists";
+import Footer from "./Footer";
+import fetchUsers from "../shared/services/userService";
+import GridStructure from "./GridStructure";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    const savedIsGrid = localStorage.getItem("isGrid"); //ovim je dohvacen local storage
+
     this.state = {
       users: [],
-      isGrid: false,
-
-    }
+      isGrid: typeof savedIsGrid !== "undefined" ? JSON.parse(savedIsGrid) : false
+    };
   }
 
   componentDidMount() {
     this.fetchAndSaveUsers();
   }
 
-  fetchAndSaveUsers = () => (
-    fetchUsers()
-      .then(users => this.setState({ users }))
-  )
+  fetchAndSaveUsers = () => fetchUsers().then(users => this.setState({ users }));
 
-  ChangingState = (e) => {
+  ChangingState = e => {
+    this.setState(prevState => {
+      const newIsGrid = !prevState.isGrid;
+      localStorage.setItem("isGrid", newIsGrid);
 
-    this.setState((prevState) => {
       return {
-        isGrid: !prevState.isGrid
-      }
-    })
-  }
+        isGrid: newIsGrid
+      };
+    });
+  };
 
   render() {
     return (
       <React.Fragment>
         <Header GridOrList={this.state.isGrid} onClickEvent={this.ChangingState} onReload={this.fetchAndSaveUsers} />
         <div className="wrapper container">
-          {
-            this.state.isGrid
-              ? <GridStructure users={this.state.users} />
-              : <UserLists users={this.state.users} />
-          }
-        </div >
+          {this.state.isGrid ? <GridStructure users={this.state.users} /> : <UserLists users={this.state.users} />}
+        </div>
         <Footer />
-      </React.Fragment >
-    )
+      </React.Fragment>
+    );
   }
 }
 
 export default App;
+
+// import React from "react";
+// import Header from "./Header";
+// import UserLists from "./UserLists";
+// import Footer from "./Footer";
+// import fetchUsers from "../shared/services/userService";
+// import GridStructure from "./GridStructure";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     const savedIsGrid = localStorage.getItem("isGrid"); //ovim je dohvacen local storage
+
+//     this.state = {
+//       users: [],
+//       isGrid: typeof savedIsGrid !== "undefined" ? JSON.parse(savedIsGrid) : false
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.fetchAndSaveUsers();
+//   }
+
+//   fetchAndSaveUsers = () => fetchUsers().then(users => this.setState({ users }));
+
+//   ChangingState = e => {
+//     this.setState(prevState => {
+
+//       const newIsGrid = !prevState.isGrid;
+
+//       localStorage.setItem("isGrid", newIsGrid);
+
+//       return {
+//         isGrid: newIsGrid
+//       };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header GridOrList={this.state.isGrid} onClickEvent={this.ChangingState} onReload={this.fetchAndSaveUsers} />
+//         <div className="wrapper container">
+//           {this.state.isGrid ? <GridStructure users={this.state.users} /> : <UserLists users={this.state.users} />}
+//         </div>
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default App;
--- Week9/BitReactUser/src/view/GridStructure.js
+++ Week9/BitReactUser/src/view/GridStructure.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './GridStructure.css'
+import "./GridStructure.css";
 
-const GridStructure = (props) => {
+const GridStructure = props => {
+  const arrOfUsers = props.users.map((oneUser, i) => {
+    return (
+      <div className="userGrid" key={i}>
+        <img src={oneUser.img} />
+        <div>
+          <p className="usernameGrid">{oneUser.setFullName()}</p>
+          <p className="emailGrid">{oneUser.hideEmail()}</p>
+          <p>Birth date {oneUser.setDate()}</p>
+        </div>
+      </div>
+    );
+  });
 
-    const arrOfUsers = props.users.map((oneUser, i) => {
-        return (
-            <div className="userGrid" key={i}>
-                <img src={oneUser.img} />
-                <div>
-                    <p className="usernameGrid">{oneUser.setFullName()}</p>
-                    <p className="emailGrid">{oneUser.hideEmail()}</p>
-                    <p>
-                        Birth date {oneUser.setDate()}
-                    </p>
-                </div>
-            </div>
-
-        )
-    })
-
-    return arrOfUsers;
-}
+  return arrOfUsers;
+};
 
 export default GridStructure;
